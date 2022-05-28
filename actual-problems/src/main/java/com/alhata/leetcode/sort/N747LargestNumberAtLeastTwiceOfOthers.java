eae48064d2809a7f9af9f1ac22905e3110362ce4
Marthevin <chengwen.ma@shopee.com>
2022-05-28 19:55:57 +0800

    commit

---
+++ actual-problems/src/main/java/com/alhata/leetcode/sort/N747LargestNumberAtLeastTwiceOfOthers.java
@@ -0,0 +1,58 @@
+package com.alhata.leetcode.sort;
+
+/**
+ * You are given an integer array nums where the largest integer is unique.
+ *
+ * Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [3,6,1,0]
+ * Output: 1
+ * Explanation: 6 is the largest integer.
+ * For every other number in the array x, 6 is at least twice as big as x.
+ * The index of value 6 is 1, so we return 1.
+ * Example 2:
+ *
+ * Input: nums = [1,2,3,4]
+ * Output: -1
+ * Explanation: 4 is less than twice the value of 3, so we return -1.
+ * Example 3:
+ *
+ * Input: nums = [1]
+ * Output: 0
+ * Explanation: 1 is trivially at least twice the value as any other number because there are no other numbers.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 50
+ * 0 <= nums[i] <= 100
+ * The largest element in nums is unique.
+ */
+public class N747LargestNumberAtLeastTwiceOfOthers {
+    public static void main(String[] args) {
+
+    }
+
+    // the key is to find the largest and second largest number in the arr
+    public int dominantIndex(int[] nums) {
+        //find max and second max
+        int max = 0, secondMax = 0, trace = 0;
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i] <= secondMax) {
+                continue;
+            }else if(nums[i] <= max) {
+                secondMax = nums[i];
+            }else {
+                secondMax = max;
+                max = nums[i];
+                trace = i;
+            }
+        }
+
+        return secondMax * 2 <= max ? trace : -1;
+    }
+}
