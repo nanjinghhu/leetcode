3142073114c9c85cb6b0666014eac556abfd81ca
Marthevin <chengwen.ma@shopee.com>
2022-06-14 20:31:07 +0800

    stack operator

---
+++ actual-problems/src/main/java/com/alhata/leetcode/stack/N227BasicCalculatorII.java
@@ -0,0 +1,74 @@
+package com.alhata.leetcode.stack;
+
+import java.util.Stack;
+
+/**
+ * Given a string s which represents an expression, evaluate this expression and return its value.
+ *
+ * The integer division should truncate toward zero.
+ *
+ * You may assume that the given expression is always valid. All intermediate results will be in the range of [-231, 231 - 1].
+ *
+ * Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "3+2*2"
+ * Output: 7
+ * Example 2:
+ *
+ * Input: s = " 3/2 "
+ * Output: 1
+ * Example 3:
+ *
+ * Input: s = " 3+5 / 2 "
+ * Output: 5
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 3 * 105
+ * s consists of integers and operators ('+', '-', '*', '/') separated by some number of spaces.
+ * s represents a valid expression.
+ * All the integers in the expression are non-negative integers in the range [0, 231 - 1].
+ * The answer is guaranteed to fit in a 32-bit integer.
+ */
+public class N227BasicCalculatorII {
+    public static void main(String[] args) {
+
+    }
+
+    public int calculate(String s) {
+        int num = 0;
+        char sign = '+';
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                num = num * 10 + s.charAt(i) - '0';
+            }
+
+            if( !Character.isDigit( s.charAt(i) ) && s.charAt(i) != ' ' || i == s.length() - 1) {
+                if(sign == '+') {
+                    stack.push(num);
+                }else if(sign == '-') {
+                    stack.push(-num);
+                }else if(sign == '*') {
+                    stack.push(stack.pop() * num);
+                }else if(sign == '/') {
+                    stack.push(stack.pop() / num);
+                }
+                sign = s.charAt(i);
+                num = 0;
+            }
+        }
+
+        int res = 0;
+        for(int ele: stack) {
+            res += ele;
+        }
+
+        return res;
+    }
+}
